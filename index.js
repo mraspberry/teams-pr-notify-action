1d66981b03f95ae4eed74e3ee22343dc382ef741
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2021-06-11 13:51:47 -0500

    Fixed broken interpolation

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 webhook_url = core.getInput('webhook-url');
-msg = "New PR Event: ${process.env.GITHUB_EVENT} |";
-msg += " Repository: ${proces.env.GITHUB_REPOSITORY} |";
-msg += " Merge target: ${proces.env.GITHUB_BASE_REF} |";
-msg += " Branch to merge: ${process.env.GITHUB_HEAD_REF}";
+msg = `New PR Event: ${process.env.GITHUB_EVENT} |`;
+msg += ` Repository: ${process.env.GITHUB_REPOSITORY} |`;
+msg += ` Merge target: ${process.env.GITHUB_BASE_REF} |`;
+msg += ` Branch to merge: ${process.env.GITHUB_HEAD_REF}`;
 axios
  .post(webhook_url, {
      text: msg
