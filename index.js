31dea7b977e08d0cffe1f8fee1d7ebf65adc5ec2
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2021-06-14 13:32:10 -0500

    Ran linting with the 'standard' tool

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 #!/usr/bin/env node
 
-const axios = require('axios');
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as ACData from 'adaptivecards-templating'
+const axios = require('axios')
+const core = require('@actions/core')
 // Pull in the JSON file via require because it doesn't change
 // And needs to be a synchronous read
-const event_data = require(process.env.GITHUB_EVENT_PATH);
-import * as ACData from "adaptivecards-templating";
-import * as AdaptiveCards from "adaptivecards";
+const eventData = require(process.env.GITHUB_EVENT_PATH)
 
-webhook_url = core.getInput('webhook-url');
-console.log(`Webhook URL: ${webhook_url}`);
-msg = `New PR Event: ${process.env.GITHUB_EVENT} |`;
-msg += ` Repository: ${process.env.GITHUB_REPOSITORY} |`;
-msg += ` Merge target: ${process.env.GITHUB_BASE_REF} |`;
-msg += ` Branch to merge: ${process.env.GITHUB_HEAD_REF}`;
+const webhookUrl = core.getInput('webhook-url')
+console.log(`Webhook URL: ${webhookUrl}`)
+// let msg = `New PR Event: ${process.env.GITHUB_EVENT} |`
+// msg += ` Repository: ${process.env.GITHUB_REPOSITORY} |`
+// msg += ` Merge target: ${process.env.GITHUB_BASE_REF} |`
+// msg += ` Branch to merge: ${process.env.GITHUB_HEAD_REF}`
 
-template_data = {
-    "type": "AdaptiveCard",
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "version": "1.3",
-    "body": [
-        {
-            "type": "TextBlock",
-            "wrap": true,
-            "text": "${$root.title}",
-            "weight": "bolder",
-            "size": "medium"
-        },
-        {
-            "type": "TextBlock",
-            "text": "${$root.description}",
-            "wrap": true
-        }
-    ],
-    "actions": [
-        {
-            "type": "Action.OpenUrl",
-            "title": "View PR",
-            "url": "${$root.url}"
-        }
-    ]
-};
-
-template = new ACData.Template(template_data);
-card_data = template.expand({
-    $root: {
-        title: `New PR for ${event_data.repository.name}`,
-        description: `New pull request opened by ${event_data.pull_request.user.login}`,
-        url: event_data.pull_request.html_url
+/* eslint-disable no-template-curly-in-string */
+const templateData = {
+  type: 'AdaptiveCard',
+  $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+  version: '1.3',
+  body: [
+    {
+      type: 'TextBlock',
+      wrap: true,
+      text: '${$root.title}',
+      weight: 'bolder',
+      size: 'medium'
+    },
+    {
+      type: 'TextBlock',
+      text: '${$root.description}',
+      wrap: true
     }
+  ],
+  actions: [
+    {
+      type: 'Action.OpenUrl',
+      title: 'View PR',
+      url: '${$root.url}'
+    }
+  ]
+}
+/* eslint-enable no-template-curly-in-string */
+
+const template = new ACData.Template(templateData)
+const cardData = template.expand({
+  $root: {
+    title: `New PR for ${eventData.repository.name}`,
+    description: `New pull request opened by ${eventData.pull_request.user.login}`,
+    url: eventData.pull_request.html_url
+  }
 })
 
 axios
- .post(webhook_url, {
-     card_data
- })
+  .post(webhookUrl, {
+    cardData
+  })
   .then(res => {
-      console.log(`statusCode': ${res.statusCode}`)
-      console.log(res)
+    console.log(`statusCode': ${res.statusCode}`)
+    console.log(res)
   })
   .catch(error => {
-        console.error(error)
+    console.error(error)
   })
